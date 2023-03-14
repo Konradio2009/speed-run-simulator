a6be4eeb365996600c0f17de48a128b209dd9b86
Konradio2009 <106486864+Konradio2009@users.noreply.github.com>
2023-03-14 18:17:41 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,1 @@
-while wait(0.1) do
-local orbs = game:GetService("Workspace").GameAssets.GlobalAssets.OrbSpawns
-for i, v in pairs(orbs:GetChildren()) do
-if v:IsA("MeshPart") then
-local user = game.Players.LocalPlayer.Character.Head
-firetouchinterest(user, v, 0)
-elseif v:IsA("Model") then
-local summerOrb = v["Orb.1"]
-firetouchinterest(game.Players.LocalPlayer.Character.Head, summerOrb, 0)
-end
-end
-game:GetService("ReplicatedStorage").Remotes.Rebirth:FireServer()
-end
+while wait(0.1) do local orbs = game:GetService("Workspace").GameAssets.GlobalAssets.OrbSpawns for i, v in pairs(orbs:GetChildren()) do if v:IsA("MeshPart") then local user = game.Players.LocalPlayer.Character.Head firetouchinterest(user, v, 0) elseif v:IsA("Model") then local summerOrb = v["Orb.1"] firetouchinterest(game.Players.LocalPlayer.Character.Head, summerOrb, 0) end end game:GetService("ReplicatedStorage").Remotes.Rebirth:FireServer() end
